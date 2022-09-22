e8fe480c9b89adfbf1e601c596e78421e442548b
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-09-22 20:07:50 +0100

    getters

--- Module_00/ex01/Contact.hpp
+++ Module_00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 20:41:32 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/09/21 16:23:31 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/09/21 17:52:52 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,18 @@
 class	Contact
 {
 	public:
-		std::string	getFirstName();
-		std::string	getLastName();
-		std::string	getNickame();
-		std::string	getphoneNumber();
-		std::string	getSecret();
+		std::string	getFirstName(void) const;
+		std::string	getLastName(void) const;
+		std::string	getNickame(void) const;
+		std::string	getphoneNumber(void) const;
+		std::string	getSecret(void) const;
 		void		setFirstName(std::string firstName);
 		void		setLastName(std::string lastName);
 		void		setNickame(std::string nickname);
 		void		setPhoneNumber(std::string nickname);
 		void		setSecret(std::string nickname);
+		// Constructors
+		Contact& operator=(const Contact&contact);
 	private:
 		std::string	FirstName;
 		std::string	LastName;

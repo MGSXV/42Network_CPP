b673b077112ee8b17c4e71489f327651bf475fee
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-09-22 20:08:15 +0100

    PhoneBook functions definition

--- Module_00/ex01/PhoneBook.cpp
+++ Module_00/ex01/PhoneBook.cpp
@@ -6,7 +6,32 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 20:41:38 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/09/20 20:41:39 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/09/21 17:57:13 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PhoneBook.hpp"
+
+PhoneBook::PhoneBook(void)
+{
+	this->ContactsNum = 0;
+}
+
+Contact PhoneBook::getContact(int index) const
+{
+	if (index < this->ContactsNum)
+		return (this->Contacts[index]);
+	std::cout << "Invalid index, please try again." << std::endl;
+	return (Contact());
+}
+
+void	PhoneBook::setContact(const Contact &contact)
+{
+	if (this->ContactsNum >= 8)
+	{
+		std::cout << "This Phone book is full!" << std::endl;
+		return ;
+	}
+	this->Contacts[this->ContactsNum] = contact;
+	this->ContactsNum++;
+}

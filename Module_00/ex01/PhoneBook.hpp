315f21994f7ee4ea1eae028ce1af22d46ee7339c
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-09-22 20:08:29 +0100

    PhoneBook Class

--- Module_00/ex01/PhoneBook.hpp
+++ Module_00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 20:41:49 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/09/21 16:24:49 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/09/21 17:49:49 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,12 @@
 class	PhoneBook
 {
 	public:
-		
+		Contact	getContact(int index) const;
+		void	setContact(const Contact &contact);
+		PhoneBook(void);
 	private:
-		Contact	contacts[8];
+		Contact	Contacts[8];
+		int		ContactsNum;
 };
 
 #endif

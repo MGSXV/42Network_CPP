9a63cfa5f2837ff047e8c568d95e4ac0c6159e4c
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-09-23 01:41:31 +0100

    get & set ContactNum

--- Module_00/ex01/PhoneBook.cpp
+++ Module_00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 20:41:38 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/09/22 20:14:00 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/09/23 01:38:43 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,16 @@
 PhoneBook::PhoneBook(void)
 {
 	this->ContactsNum = 0;
+}
+
+int	PhoneBook::getContactNum(void) const
+{
+	return (this->ContactsNum);
+}
+
+void	PhoneBook::setContactNum(int i)
+{
+	this->ContactsNum = i;
 }
 
 Contact PhoneBook::getContact(int index) const
@@ -27,12 +37,47 @@ Contact PhoneBook::getContact(int index) const
 
 void	PhoneBook::setContact(const Contact &contact)
 {
-	if (this->ContactsNum >= 8)
+	this->Contacts[this->ContactsNum] = contact;
+	this->setContactNum(this->ContactsNum + 1);
+	std::cout << "New contact was created!" << std::endl;
+}
+
+std::string PhoneBook::formatDisplay(std::string str)
+{
+	std::string	width;
+	int			i;
+
+	if (str.length() <= 10)
 	{
-		std::cout << "This Phone book is full!" << std::endl;
+		i = -1;
+		width = "";
+		while (++i < (int) (10 - str.length()))
+			width += " ";
+		return (str + width);
+	}
+	str.resize(9);
+	return (str + ".");
+}
+
+void	PhoneBook::displayContacts(void)
+{
+	int	i;
+
+	i = -1;
+	if (!this->ContactsNum)
+	{
+		std::cout << "There are no contacts to display!" << std::endl;
 		return ;
 	}
-	this->Contacts[this->ContactsNum] = contact;
-	this->ContactsNum++;
-	std::cout << "New contact was created!" << std::endl;
+	std::cout << "|Index     |First name|Last name |Nickname  |" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	while (++i < this->ContactsNum)
+	{
+		std::cout << "|";
+		std::cout << i + 1 << std::setw(10) << "|";
+		std::cout << this->formatDisplay(this->Contacts[i].getFirstName()) << "|";
+		std::cout << this->formatDisplay(this->Contacts[i].getLastName()) << "|";
+		std::cout << this->formatDisplay(this->Contacts[i].getNickname()) << "|";
+		std::cout << std::endl;
+	}
 }

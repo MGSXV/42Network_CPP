8fdc999a35838a3a038327bbf73dc28094837aaa
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-09-23 01:41:02 +0100

    Fixing typos

--- Module_00/ex01/Contact.cpp
+++ Module_00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 16:30:33 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/09/22 20:11:30 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/09/23 00:33:40 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ Contact&	Contact::operator=(const Contact&contact)
 {
 	this->FirstName = contact.getFirstName();
 	this->LastName = contact.getLastName();
-	this->Nickname = contact.getNickame();
+	this->Nickname = contact.getNickname();
 	this->PhoneNumber = contact.getphoneNumber();
 	this->Secret = contact.getSecret();
 	return (*this);
@@ -32,7 +32,7 @@ std::string	Contact::getLastName() const
 	return (this->LastName);
 }
 
-std::string	Contact::getNickame() const
+std::string	Contact::getNickname() const
 {
 	return (this->Nickname);
 }
@@ -57,7 +57,7 @@ void	Contact::setLastName(std::string lastName)
 	this->LastName = lastName;
 }
 
-void	Contact::setNickame(std::string nickname)
+void	Contact::setNickname(std::string nickname)
 {
 	this->Nickname = nickname;
 }

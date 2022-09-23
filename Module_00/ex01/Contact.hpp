78d43bcad42bbd1bdce406c8af9c7844df83441e
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-09-23 01:41:12 +0100

    typos

--- Module_00/ex01/Contact.hpp
+++ Module_00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 20:41:32 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/09/22 23:29:23 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/09/23 00:33:05 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 # define CONTACT_HPP
 
 #include <iostream>
+#include <iomanip>
 
 class	Contact
 {
 	public:
 		std::string	getFirstName(void) const;
 		std::string	getLastName(void) const;
-		std::string	getNickame(void) const;
+		std::string	getNickname(void) const;
 		std::string	getphoneNumber(void) const;
 		std::string	getSecret(void) const;
 		void		setFirstName(std::string firstName);
 		void		setLastName(std::string lastName);
-		void		setNickame(std::string phoneNumber);
+		void		setNickname(std::string phoneNumber);
 		void		setPhoneNumber(std::string nickname);
 		void		setSecret(std::string secret);
 		// Constructors

d3dc5b16b542a0ea768c25a5dca8007efe3bb025
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-09-24 23:48:47 +0100

    Fix main

--- Module_01/ex01/main.cpp
+++ Module_01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 19:22:37 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/09/24 23:46:17 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/09/24 23:48:42 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int	main(void)
 	Zombie	zombie1;
 	Zombie	*zombies;
 
-	std::cout << sizeof(Zombie) << std::endl;
 	zombie1.setName("Creeper");
 	zombie1.announce();
 	zombies = zombieHorde(5, "Creeper");

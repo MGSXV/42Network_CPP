9ab07f6499d0c5dbc7a8bababd77faf69a9d3711
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-09-27 17:36:11 +0100

    Fix

--- Module_00/ex00/Megaphone.cpp
+++ Module_00/ex00/Megaphone.cpp
@@ -6,13 +6,11 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 03:13:40 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/09/12 21:22:22 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/09/27 17:33:55 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Megaphone.hpp"
 #include <iostream>
-using std::cout;
 
 void	str_toupper(char *str)
 {
@@ -27,12 +25,12 @@ int main(int c, char **v)
 
 	if (c <= 1)
 	{
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
 	i = 0;
 	while (v[++i])
 		str_toupper(v[i]);
-	cout << std::endl;
+	std::cout << std::endl;
 	return (0);
 }

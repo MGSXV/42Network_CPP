dc2ff45b3ae53276b038f73294b67bac2dc18496
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-10-04 21:38:52 +0100

    Base code for Fixed class

--- Module_02/ex03/Fixed.cpp
+++ Module_02/ex03/Fixed.cpp
@@ -6,7 +6,172 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 21:33:24 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/10/04 21:33:25 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/10/04 21:38:36 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+const int	Fixed::fractions = 8;
+
+// Constructors and destructors
+Fixed::Fixed(void)
+{
+	this->setRawBit(0);
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	*this = other;
+}
+
+Fixed::Fixed(const int value)
+{
+	this->setRawBit(value << Fixed::fractions);
+}
+
+Fixed::Fixed(const float value)
+{
+	this->setRawBit(roundf(value * (1 << Fixed::fractions)));
+}
+
+Fixed::~Fixed(void)
+{
+}
+
+// Operators overload
+Fixed	&Fixed::operator=(const Fixed& fixed)
+{
+	this->setRawBit(fixed.raw);
+	return (*this);
+}
+
+std::ostream	&operator<<(std::ostream &out,const Fixed& fixed)
+{
+	out << fixed.toFloat();
+	return (out);
+}
+// Comparison operators
+bool	Fixed::operator==(const Fixed &left) const
+{
+	return (this->toFloat() == left.toFloat());
+}
+
+bool	Fixed::operator!=(const Fixed &left) const
+{
+	return (this->toFloat() != left.toFloat());
+}
+
+bool	Fixed::operator>(const Fixed &left) const
+{
+	return (this->toFloat() > left.toFloat());
+}
+
+bool	Fixed::operator<(const Fixed &left) const
+{
+	return (this->toFloat() < left.toFloat());
+}
+
+bool	Fixed::operator>=(const Fixed &left) const
+{
+	return (this->toFloat() >= left.toFloat());
+}
+
+bool	Fixed::operator<=(const Fixed &left) const
+{
+	return (this->toFloat() <= left.toFloat());
+}
+// Arcmitic operators
+Fixed	Fixed::operator++(void)
+{
+	++this->raw;
+	return (*this);
+}
+
+Fixed	Fixed::operator--(void)
+{
+	--this->raw;
+	return (*this);
+}
+
+Fixed	Fixed::operator++(int)
+{
+	Fixed	tmp(*this);
+	
+	this->raw++;
+	return (tmp);
+}
+
+Fixed	Fixed::operator--(int)
+{
+	Fixed	tmp(*this);
+	
+	this->raw--;
+	return (tmp);
+}
+
+Fixed	Fixed::operator+(Fixed &other)
+{
+	Fixed	tmp(this->toFloat() + other.toFloat());
+	return (tmp);
+}
+
+Fixed	Fixed::operator-(Fixed &other)
+{
+	Fixed	tmp(this->toFloat() - other.toFloat());
+	return (tmp);
+}
+
+Fixed	Fixed::operator*(const Fixed &other)
+{
+	Fixed	tmp(this->toFloat() * other.toFloat());
+	return (tmp);
+}
+
+Fixed	Fixed::operator/(Fixed &other)
+{
+	Fixed	tmp(this->toFloat() / other.toFloat());
+	return (tmp);
+}
+
+int	Fixed::getRawBit(void) const
+{
+	return (this->raw);
+}
+
+void	Fixed::setRawBit(const int value)
+{
+	this->raw = value;
+}
+
+int	Fixed::toInt(void) const
+{
+	return (this->raw / (1 << Fixed::fractions));
+}
+
+float	Fixed::toFloat(void) const
+{
+	return ((float) this->raw / (1 << Fixed::fractions));
+}
+
+Fixed	&Fixed::min(Fixed &p1, Fixed &p2)
+{
+	return ((p1 < p2) ? p1 : p2);
+}
+
+const Fixed	&Fixed::min(const Fixed &p1, const Fixed &p2)
+{
+	const Fixed	&tmp = (p1 < p2) ? p1 : p2;
+	return (tmp);
+}
+
+Fixed	&Fixed::max(Fixed &p1, Fixed &p2)
+{
+	return ((p1 > p2) ? p1 : p2);
+}
+
+const Fixed	&Fixed::max(const Fixed &p1, const Fixed &p2)
+{
+	const Fixed	&tmp = (p1 > p2) ? p1 : p2;
+	return (tmp);
+}
--- Module_02/ex03/Fixed.hpp
+++ Module_02/ex03/Fixed.hpp
@@ -6,7 +6,61 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 21:33:20 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/10/04 21:33:21 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/10/04 21:38:20 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#ifndef __FIXED_HPP__
+# define __FIXED_HPP__
+
+# include <iostream>
+# include <cmath>
+
+class	Fixed
+{
+	public:
+		// Constructors and destructors
+		Fixed();
+		Fixed(const Fixed &other);
+		Fixed(const int num);
+		Fixed(const float num);
+		~Fixed();
+		// Operators overload
+		// // Comparison operators
+		bool	operator==(const Fixed &left) const;
+		bool	operator!=(const Fixed &left) const;
+		bool	operator>(const Fixed &left) const;
+		bool	operator<(const Fixed &left) const;
+		bool	operator>=(const Fixed &left) const;
+		bool	operator<=(const Fixed &left) const;
+		// // Arcmitic operators
+		Fixed	operator++(void);
+		Fixed	operator--(void);
+		Fixed	operator++(int);
+		Fixed	operator--(int);
+		Fixed	operator+(Fixed &other);
+		Fixed	operator-(Fixed &other);
+		Fixed	operator/(Fixed &other);
+		Fixed	operator*(const Fixed &other);
+		// // Copy assignment operator
+		Fixed	&operator=(const Fixed &other);
+		// Getters and setters
+		void	setRawBit(int const raw);
+		int		getRawBit(void) const;
+		// Member functions
+		float			toFloat(void) const;
+		int				toInt(void) const;
+		static Fixed	&min(Fixed &p1, Fixed &p2);
+		static const Fixed	&min(const Fixed &p1, const Fixed &p2);
+		static Fixed	&max(Fixed &p1, Fixed &p2);
+		static const Fixed	&max(const Fixed &p1, const Fixed &p2);
+	private:
+		int					raw;
+		static const int	fractions;
+};
+
+std::ostream	&operator<<(std::ostream &obj, const Fixed& fixed);
+
+
+#endif

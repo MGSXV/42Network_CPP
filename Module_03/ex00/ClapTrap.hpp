15aeee4f9e114c5d2398ff7ead3a8f7cf8b6c211
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-10-07 20:36:20 +0100

    Class prototype

--- Module_03/ex00/ClapTrap.hpp
+++ Module_03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 20:19:50 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/10/07 20:22:00 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/10/07 20:35:43 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,37 @@
 #ifndef __CLAPTRAP_HPP__
 # define __CLAPTRAP_HPP__
 
+# include <iostream>
+
+typedef std::string	str_t;
+
 class ClapTrap
 {
 	private:
-		
+		str_t	Name;
+		int		HitPoints;
+		int		EnergyPoints;
+		int		AttackDamage;
 	public:
-		
+		// Constructors and destructors
+		ClapTrap(void);
+		~ClapTrap();
+		ClapTrap(const ClapTrap &other);
+		// Assignment operator overload
+		ClapTrap&	operator=(const ClapTrap &other);
+		// Getters and setters
+		str_t	getName(void);
+		int		getHitpoints(void);
+		int		getEnergyPoints(void);
+		int		getAttackDamage(void);
+		void	setName(str_t name);
+		void	setHitpoints(int hitPoints);
+		void	setEnergyPoints(int energyPoints);
+		void	setAttackDamage(int attackDamage);
+		// Member functions
+		void	attack(const str_t &target);
+		void	takeDamage(unsigned int damage);
+		void	beRepaired(unsigned int amount);
 };
 
 #endif

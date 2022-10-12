1f28ef7eae743590972a00cc3976dc93b29b6629
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-10-12 05:43:30 +0100

    clean the code a lil bet

--- Module_04/ex00/Animal.hpp
+++ Module_04/ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 05:15:47 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/10/12 05:26:39 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/10/12 05:43:19 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ class Animal
 		str_t	type;
 	public:
 		// Constructors and destructors
-		Animal();
-        ~Animal();
+		Animal(void);
+        ~Animal(void);
 		Animal(const Animal& animal);
 		// Assignment operator overload
 		Animal& operator=(const Animal& animal);
 		// Getters and setters
-		str_t	getType() const;
+		str_t	getType(void) const;
 		void	setType(const str_t& t);
 };
 

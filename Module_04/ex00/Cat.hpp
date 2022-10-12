45418431aae19308ebfc40ba03013cb1b36e0700
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-10-12 05:22:20 +0100

    Create cat files

---
+++ Module_04/ex00/Cat.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 05:21:35 by sel-kham          #+#    #+#             */
+/*   Updated: 2022/10/12 05:22:04 by sel-kham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef __CAT_HPP__
+# define __CAT_HPP__
+
+# include <iostream>
+# include "Animal.hpp"
+
+typedef std::string	str_t;
+
+class Cat : public Animal
+{
+	private:
+	protected:
+	public:
+};
+
+#endif

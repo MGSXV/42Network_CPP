1f2c207f2ca397a100ec30aa33d654f8efa6c3e3
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-10-12 05:32:26 +0100

    Animal class is done

--- Module_04/ex00/Animal.cpp
+++ Module_04/ex00/Animal.cpp
@@ -6,7 +6,32 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 05:15:41 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/10/12 05:15:42 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/10/12 05:31:26 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Animal.hpp"
+
+// Constructors and destructors
+Animal::Animal(void)
+{
+	this->setType("Nothing yet!");
+	std::cout << "Hello from Animal!" << std::endl;
+}
+
+Animal::~Animal(void)
+{
+	std::cout << "Goodbye from Animal!" << std::endl;
+}
+
+Animal::Animal(const Animal& animal)
+{
+	*this = animal;
+}
+
+// Assignment operator overload
+Animal& Animal::operator=(const Animal& animal)
+{
+	this->setType(animal.getType());
+	return (*this);
+}
--- Module_04/ex00/Animal.hpp
+++ Module_04/ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 05:15:47 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/10/12 05:21:11 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/10/12 05:26:39 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,18 @@ typedef std::string	str_t;
 
 class Animal
 {
-	private:
 	protected:
 		str_t	type;
 	public:
+		// Constructors and destructors
+		Animal();
+        ~Animal();
+		Animal(const Animal& animal);
+		// Assignment operator overload
+		Animal& operator=(const Animal& animal);
+		// Getters and setters
+		str_t	getType() const;
+		void	setType(const str_t& t);
 };
 
 #endif

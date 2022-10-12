af8452f9189482e165ce7667aad2e8bd504475c2
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-10-12 05:20:46 +0100

    Animal class is ready

---
+++ Module_04/ex00/Animal.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 05:15:47 by sel-kham          #+#    #+#             */
+/*   Updated: 2022/10/12 05:18:08 by sel-kham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef __ANIMAL_HPP__
+# define __ANIMAL_HPP__
+
+# include <iostream>
+
+typedef std::string	str_t;
+
+class Animal
+{
+	private:
+	protected:
+	str_t	type;
+	public:
+};
+
+#endif

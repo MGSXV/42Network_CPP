c9011c1f7483cca5cbebb1a7d0b6c9c7f13562fa
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-10-12 05:20:57 +0100

    Basic files

---
+++ Module_04/ex00/Dog.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 05:19:23 by sel-kham          #+#    #+#             */
+/*   Updated: 2022/10/12 05:20:13 by sel-kham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef __DOG_HPP__
+# define __DOG_HPP__
+
+# include <iostream>
+# include "Animal.hpp"
+
+typedef std::string	str_t;
+
+class Dog : public Animal
+{
+	private:
+	protected:
+	public:
+};
+
+#endif

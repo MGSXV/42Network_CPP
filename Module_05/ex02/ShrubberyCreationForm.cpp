d3d8c45aba079d753eb7b2c0bbf6eef5eaac38ae
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-10-22 05:26:03 +0100

    ShrubberyCreationForm class is almost done

--- Module_05/ex02/ShrubberyCreationForm.cpp
+++ Module_05/ex02/ShrubberyCreationForm.cpp
@@ -5,76 +5,44 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/19 01:56:43 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/10/21 22:19:41 by sel-kham         ###   ########.fr       */
+/*   Created: 2022/10/22 04:27:39 by sel-kham          #+#    #+#             */
+/*   Updated: 2022/10/22 05:25:44 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ShrubberyCreationForm.hpp"
 
-ShrubberyCreationForm::ShrubberyCreationForm(void) : name("chahadat lwafat"), minSignGrade(145), minExecGrade(5137)
+ShrubberyCreationForm::ShrubberyCreationForm(void) : Form("ShrubberyCreationForm", false, 145, 137)
 {
-	this->_isSigned = false;
-}
-
-ShrubberyCreationForm::ShrubberyCreationForm(str_t target) : name("chahadat lwafat"), minSignGrade(145), minExecGrade(5137)
-{
-	this->_isSigned = false;
-	this->target = target;
+	this->setTarget("default target");
 }
 
 ShrubberyCreationForm::~ShrubberyCreationForm(void) { }
 
-ShrubberyCreationForm::ShrubberyCreationForm(const str_t& name, str_t target) : name(name), minSignGrade(145), minExecGrade(137)
+ShrubberyCreationForm::ShrubberyCreationForm(str_t target) : Form("ShrubberyCreationForm", false, 145, 137)
 {
-	this->_isSigned = false;
-	this->target = target;
+	this->setTarget(target);
 }
 
-ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm & other) : name(other.name), minSignGrade(other.minSignGrade), minExecGrade(other.minExecGrade)
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &cpy): Form(cpy.getName(), cpy.isSigned(), cpy.getMinGradeToSign(), cpy.getMinGradeToExec())
 {
-	this->_isSigned = other._isSigned;
-	this->target = other.target;
+	this->setTarget(cpy.getTarget());
 }
 
 // Assignment operator overload
-ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm & other)
+ShrubberyCreationForm &ShrubberyCreationForm::operator=(const ShrubberyCreationForm &cpy)
 {
-	return (*(new ShrubberyCreationForm(other)));
+	ShrubberyCreationForm*	newSCF = new ShrubberyCreationForm(cpy);
+	return (*newSCF);
 }
 
-// Member functions
-void	ShrubberyCreationForm::execute(Bureaucrat const & executor) const
+// Getters and setters
+void	ShrubberyCreationForm::setTarget(const str_t target)
 {
-	std::ofstream	treeFile;
-	if (this->_isSigned)
-	{
-		if (executor.getGrade() > this->getMinSignGrade())
-		{
-			throw Form::GradeTooLowException();
-		}
-		treeFile.open(this->target + "_signature", std::ios_base::app);
-		if (!treeFile)
-		{
-			std::cout << "\033[0;31mError opening destination file!\033[0;37m" << std::endl;
-			exit(1);
-		}
-		treeFile << "                                  # #### ####" << std::endl;
-		treeFile << "                                ### \\/#|### |/####" << std::endl;
-		treeFile << "                               ##\\/#/ \\||/##/_/##/_#" << std::endl;
-		treeFile << "                             ###  \\/###|/ \\/ # ###" << std::endl;
-		treeFile << "                           ##_\\_#\\_\\## | #/###_/_####" << std::endl;
-		treeFile << "                          ## #### # \\ #| /  #### ##/##" << std::endl;
-		treeFile << "                           __#_--###`  |{,###---###-~" << std::endl;
-		treeFile << "                                     \\ }{" << std::endl;
-		treeFile << "                                      }}{" << std::endl;
-		treeFile << "                                      }}{" << std::endl;
-		treeFile << "                                      {{}" << std::endl;
-		treeFile << "                                , -=-~{ .-^- _" << std::endl;
-		treeFile << "                                      `}" << std::endl;
-		treeFile << "                                      `}" << std::endl;
-		treeFile.close();
-	}
-	else
-		std::cerr << "\033[0;31mThis form is not signed!\033[0;37m" << std::endl;
+	this->target = target;
+}
+
+str_t    ShrubberyCreationForm::getTarget(void) const
+{
+	return (this->target);
 }
--- Module_05/ex02/ShrubberyCreationForm.hpp
+++ Module_05/ex02/ShrubberyCreationForm.hpp
@@ -5,45 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/19 01:58:57 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/10/21 21:19:04 by sel-kham         ###   ########.fr       */
+/*   Created: 2022/10/22 04:24:57 by sel-kham          #+#    #+#             */
+/*   Updated: 2022/10/22 05:23:55 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-#ifndef __SHRUBBERYCREATIONFORM_HPP__
-# define __SHRUBBERYCREATIONFORM_HPP__
+#ifndef __SHRUBBERYCREATIONFORM_H__
+# define __SHRUBBERYCREATIONFORM_H__
 
 # include <iostream>
 # include <exception>
-# include <fstream>
 # include "Form.hpp"
-# include "Bureaucrat.hpp"
-
-class Bureaucrat;
 
 typedef std::string	str_t;
 
-class ShrubberyCreationForm : public Form
+class	ShrubberyCreationForm : public Form
 {
 	private:
-		const str_t			name;
-		bool				_isSigned;
-		const unsigned int	minSignGrade;
-		const unsigned int	minExecGrade;
-		str_t				target;
-    public:
+		str_t	target;
+	public:
 		// Constructors and destructors
 		ShrubberyCreationForm(void);
-		ShrubberyCreationForm(str_t target);
-		ShrubberyCreationForm(const str_t& name, str_t target);
 		~ShrubberyCreationForm(void);
-		ShrubberyCreationForm(const str_t& name);
-		ShrubberyCreationForm(const ShrubberyCreationForm & other);
+		ShrubberyCreationForm(str_t target);
+		ShrubberyCreationForm(const ShrubberyCreationForm &cpy);
 		// Assignment operator overload
-		ShrubberyCreationForm& operator=(const ShrubberyCreationForm & other);
-		// Member functions
-		void	execute(Bureaucrat const & executor) const;
+		ShrubberyCreationForm &operator=(const ShrubberyCreationForm &cpy);
+		// Getters and setters
+		void	setTarget(const str_t target);
+		str_t    getTarget(void) const;
 };
 
 #endif
